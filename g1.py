48ab8e86b20dfa2d33ce36edf3645de371937c56
liber1988 <92869703+liber1988@users.noreply.github.com>
2026-03-26 14:28:56 +0200

    G1.py

---
+++ g1.py
@@ -0,0 +1,21 @@
+def my_generator():
+    print("Start")
+
+    try:
+        yield 1
+        print("After first yield")
+
+        yield 2
+        print("After second yield")
+
+    except Exception as e:
+        print("Caught inside generator:", e)
+
+    print("End")
+user=my_generator()
+print(next(user))
+print(user.__next__())
+# print(user.__next__())
+user.throw(Exception("An error occurred"))
+
+
